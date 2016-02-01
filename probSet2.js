18c60a9d5b82d301aa26261cd785b11a28c41443
dslester <dsles@dslesters-MacBook-Air.local>
2016-01-31 23:26:45 -0700

    probSet2_2

--- probSet2.js
+++ probSet2.js
@@ -1,19 +1,41 @@
 //---------------------------------------
 
-var str="reverse"
+// var str="reverse"
 
-function firstReverse (string) {
-	array = string.split('')
-	newArray = []
-	for (i=array.length-1; i>=0; i-=1) {
-		newArray.push(array[i]);
+// // function firstReverse (string) {
+// // 	var array = string.split('')
+// // 	newArray = []
+// // 	for (i=array.length-1; i>=0; i-=1) {
+// // 		newArray.push(array[i]);
+// // 	}
+// // 	newString = newArray.join('');
+// // 	return newString;
+// // }
+
+// // testIt = firstReverse(str);
+// // console.log(testIt);
+
+//---------------------------------------
+
+
+var str="swapCASEa"
+
+function swapCase (string) {
+	var array = string.split('')
+	for (i=0; i<array.length; i++) {
+		if (array[i] === array[i].toLowerCase()) {
+			array[i] = array[i].toUpperCase();
+		}
+		else {
+			array[i] = array[i].toLowerCase();
+		}
 	}
-	newString = newArray.join('');
+	newString = array.join('');
 	return newString;
 }
-
-testIt = firstReverse(str);
+testIt = swapCase(str);
 console.log(testIt);
+
 
 //---------------------------------------
 
